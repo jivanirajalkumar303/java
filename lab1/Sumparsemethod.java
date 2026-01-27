5e181c450f94f3cb4c020785c4173f64b4fdccaf
jivanirajalkumar303 <jivanirajalkumar303@gmail.com>
2026-01-27 17:04:37 +0530

    Delete lab1 directory

--- lab1/Sumparsemethod.java
+++
@@ -1,9 +0,0 @@
-public class Sumparsemethod {
-
-    public static void main(String[] args) {
-        int a=Integer.parseInt(args[0]);
-        int b=Integer.parseInt(args[1]);
-        System.out.println("sum: "+ (a+b));
-
-    }
-}
